d878af0d99408196add0c2dbb240d826d7c9d86e
james <jamesprovee@gmail.com>
2025-03-07 12:02:25 -0700

    Initial dashboard

---
+++ BakeryWebApp/Controllers/DashboardController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BakeryWebApp.Controllers
+{
+    public class DashboardController : Controller
+    {
+        public IActionResult Index(string activeCategory = "all")
+        {
+            return View();
+        }
+    }
+}
