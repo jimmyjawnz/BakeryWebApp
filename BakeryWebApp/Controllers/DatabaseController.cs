309bcf2f928fc3be7848e549f596275a33fbeb30
james <jamesprovee@gmail.com>
2025-03-07 13:11:41 -0700

    Product List and Database Controller and View start up

---
+++ BakeryWebApp/Controllers/DatabaseController.cs
@@ -0,0 +1,48 @@
+﻿using BakeryWebApp.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BakeryWebApp.Controllers
+{
+    public class DatabaseController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult ProductList()
+        {
+            DatabaseViewModel viewModel = new() { 
+                Products =
+                [
+                    new Product() { 
+                        ProductId = 1,
+                        ProductName = "White Bread",
+                        ProductPrice = 10.0d,
+                        ProductDescription = "Loaf of white bread.",
+                        GroupId = 1,
+                        IsAvailable = true
+                    },
+                    new Product() {
+                        ProductId = 2,
+                        ProductName = "Cheese Buns",
+                        ProductPrice = 14.0d,
+                        ProductDescription = "4 pack of buns.",
+                        GroupId = 2,
+                        IsAvailable = true
+                    },
+                    new Product() {
+                        ProductId = 3,
+                        ProductName = "Slime",
+                        ProductPrice = 99999.99d,
+                        ProductDescription = "Ew gross.",
+                        GroupId = 9,
+                        IsAvailable = false
+                    }
+                ]
+            };
+            return View(viewModel);
+        }
+    }
+}
---
+++ BakeryWebApp/Models/DatabaseViewModel.cs
@@ -0,0 +1,7 @@
+﻿namespace BakeryWebApp.Models
+{
+    public class DatabaseViewModel
+    {
+        public List<Product> Products { get; set; } = [];
+    }
+}
